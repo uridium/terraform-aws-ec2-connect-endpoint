993571588717d28491a2d1ff01854ab7db579c70
Radar <radar@protonmail.com>
2023-08-08 11:32:38 +0200

    Add output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,24 @@
 output "id" {
-  value = aws_ec2_instance_connect_endpoint.this.id
+  description = "The ID of the EC2 Connect Endpoint"
+  value       = aws_ec2_instance_connect_endpoint.this.id
 }
 
 output "subnet_id" {
-  value = aws_ec2_instance_connect_endpoint.this.subnet_id
+  description = "The ID of the endpoint subnet"
+  value       = aws_ec2_instance_connect_endpoint.this.subnet_id
 }
 
 output "security_group_ids" {
-  value = aws_ec2_instance_connect_endpoint.this.security_group_ids
+  description = "One or more security groups associated with the endpoint"
+  value       = aws_ec2_instance_connect_endpoint.this.security_group_ids
 }
 
 output "availability_zone" {
-  value = aws_ec2_instance_connect_endpoint.this.availability_zone
-}
-
-output "network_interface_ids" {
-  value = aws_ec2_instance_connect_endpoint.this.network_interface_ids
+  description = "The availability zone of the endpoint"
+  value       = aws_ec2_instance_connect_endpoint.this.availability_zone
 }
 
 output "vpc_id" {
-  value = aws_ec2_instance_connect_endpoint.this.vpc_id
+  description = "The VPC ID in which the endpoint is created"
+  value       = aws_ec2_instance_connect_endpoint.this.vpc_id
 }
